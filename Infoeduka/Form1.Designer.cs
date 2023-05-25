65cc2cf1700f3d078d2f8cc4211a62b846bb05d9
Franko Bozic <fraanko3721@gmail.com>
2023-05-25 13:30:35 +0200

    first commit

---
+++ Infoeduka/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Infoeduka
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.notificationsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.coursesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlNotifications = new System.Windows.Forms.Panel();
+            this.flpNotifications = new System.Windows.Forms.FlowLayoutPanel();
+            this.pbxAdd = new System.Windows.Forms.PictureBox();
+            this.lbHeader = new System.Windows.Forms.Label();
+            this.pbxLogo = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.pnlNotifications.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxAdd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.notificationsToolStripMenuItem,
+            this.usersToolStripMenuItem,
+            this.coursesToolStripMenuItem,
+            this.logOutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(882, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // notificationsToolStripMenuItem
+            // 
+            this.notificationsToolStripMenuItem.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.notificationsToolStripMenuItem.Name = "notificationsToolStripMenuItem";
+            this.notificationsToolStripMenuItem.Size = new System.Drawing.Size(108, 24);
+            this.notificationsToolStripMenuItem.Text = "Notifications";
+            // 
+            // usersToolStripMenuItem
+            // 
+            this.usersToolStripMenuItem.Name = "usersToolStripMenuItem";
+            this.usersToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+            this.usersToolStripMenuItem.Text = "Users";
+            // 
+            // coursesToolStripMenuItem
+            // 
+            this.coursesToolStripMenuItem.Name = "coursesToolStripMenuItem";
+            this.coursesToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.coursesToolStripMenuItem.Text = "Courses";
+            // 
+            // logOutToolStripMenuItem
+            // 
+            this.logOutToolStripMenuItem.Name = "logOutToolStripMenuItem";
+            this.logOutToolStripMenuItem.Size = new System.Drawing.Size(74, 24);
+            this.logOutToolStripMenuItem.Text = "Log out";
+            // 
+            // pnlNotifications
+            // 
+            this.pnlNotifications.Controls.Add(this.flpNotifications);
+            this.pnlNotifications.Controls.Add(this.pbxAdd);
+            this.pnlNotifications.Controls.Add(this.lbHeader);
+            this.pnlNotifications.Controls.Add(this.pbxLogo);
+            this.pnlNotifications.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlNotifications.Location = new System.Drawing.Point(0, 28);
+            this.pnlNotifications.Name = "pnlNotifications";
+            this.pnlNotifications.Size = new System.Drawing.Size(882, 525);
+            this.pnlNotifications.TabIndex = 1;
+            // 
+            // flpNotifications
+            // 
+            this.flpNotifications.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.flpNotifications.Location = new System.Drawing.Point(12, 103);
+            this.flpNotifications.Name = "flpNotifications";
+            this.flpNotifications.Size = new System.Drawing.Size(858, 410);
+            this.flpNotifications.TabIndex = 3;
+            // 
+            // pbxAdd
+            // 
+            this.pbxAdd.Image = global::Infoeduka.Properties.Resources.png_transparent_computer_icons_symbol_addition_plus_and_minus_signs_plus_miscellaneous_logo_plus;
+            this.pbxAdd.Location = new System.Drawing.Point(825, 35);
+            this.pbxAdd.Name = "pbxAdd";
+            this.pbxAdd.Size = new System.Drawing.Size(45, 38);
+            this.pbxAdd.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxAdd.TabIndex = 2;
+            this.pbxAdd.TabStop = false;
+            // 
+            // lbHeader
+            // 
+            this.lbHeader.AutoSize = true;
+            this.lbHeader.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbHeader.Location = new System.Drawing.Point(364, 35);
+            this.lbHeader.Name = "lbHeader";
+            this.lbHeader.Size = new System.Drawing.Size(173, 38);
+            this.lbHeader.TabIndex = 1;
+            this.lbHeader.Text = "Notifications";
+            this.lbHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pbxLogo
+            // 
+            this.pbxLogo.Image = global::Infoeduka.Properties.Resources.visoko_učilište_algebra_logo_782;
+            this.pbxLogo.Location = new System.Drawing.Point(3, 3);
+            this.pbxLogo.Name = "pbxLogo";
+            this.pbxLogo.Size = new System.Drawing.Size(119, 94);
+            this.pbxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxLogo.TabIndex = 0;
+            this.pbxLogo.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 553);
+            this.Controls.Add(this.pnlNotifications);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Infoeduka";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.pnlNotifications.ResumeLayout(false);
+            this.pnlNotifications.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxAdd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem notificationsToolStripMenuItem;
+        private ToolStripMenuItem usersToolStripMenuItem;
+        private ToolStripMenuItem coursesToolStripMenuItem;
+        private ToolStripMenuItem logOutToolStripMenuItem;
+        private Panel pnlNotifications;
+        private PictureBox pbxLogo;
+        private Label lbHeader;
+        private PictureBox pbxAdd;
+        private FlowLayoutPanel flpNotifications;
+    }
+}
