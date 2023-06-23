3bafcbd899b46f92e555f5ee2071a3ed2e1e8342
Robert Rnjak-Vuksan <robertrnjakvuksan@gmail.com>
2023-06-23 22:37:38 +0200

    add users and courses implementation

--- Infoeduka/AddUser.cs
+++ Infoeduka/AddUser.cs
@@ -13,5 +13,46 @@ namespace Infoeduka
 {
     public partial class AddUser : Form
     {
+        private const string PATHUSERS = @"../../../Data/Users.txt";
+
+        public AddUser()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            User user = new User();
+            user.Email = tbEmail.Text;
+            user.Password = tbPassword.Text;
+            if (rbAdmin.Checked == true)
+            {
+                user.Role = rbAdmin.Text;
+            }
+            else
+            {
+                user.Role = rbLecturer.Text;
+            }
+
+            if (!File.Exists(PATHUSERS))
+            {
+                File.Create(PATHUSERS);
+            }
+
+            File.AppendAllTextAsync(PATHUSERS, User.SetForFileLine(user) + "\n");
+            this.Hide();
+        }
+
+        private void rbLecturer_Click(object sender, EventArgs e)
+        {
+            rbAdmin.Checked = false;
+            rbLecturer.Checked = true;
+        }
+
+        private void rbAdmin_Click(object sender, EventArgs e)
+        {
+            rbAdmin.Checked = true;
+            rbLecturer.Checked = false;
+        }
     }
 }
--- Infoeduka/Courses.cs
+++ Infoeduka/Courses.cs
@@ -1,4 +1,5 @@
-﻿using Infoeduka.Model;
+﻿
+using Infoeduka.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,5 +14,75 @@ namespace Infoeduka
 {
     public partial class Courses : Form
     {
+        private const string LOGROLEPATH = @"../../../Data/LogRole.txt";
+        private const string PATHCOURSES = @"../../../Data/Courses.txt";
+        private const char DEL = '|';
+
+        public Courses()
+        {
+            InitializeComponent();
+            string log = File.ReadAllText(LOGROLEPATH);
+            if (log != "Admin")
+            {
+                usersToolStripMenuItem.Visible = false;
+            }
+        }
+
+        private void notificationsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1 main = new Form1();
+            main.Show();
+            this.Hide();
+        }
+
+        private void usersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Users users = new Users();
+            users.Show();
+            this.Hide();
+        }
+
+        private void logOutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            this.Hide();
+        }
+
+        private void Courses_Activated(object sender, EventArgs e)
+        {
+            LoadCourses();
+        }
+
+        private void LoadCourses()
+        {
+            lbCourses.Items.Clear();
+
+            if (!File.Exists(PATHCOURSES))
+            {
+                File.Create(PATHCOURSES);
+            }
+
+            string[] lines = File.ReadAllLines(PATHCOURSES);
+            List<Course> courses = new List<Course>();
+            foreach (string line in lines)
+            {
+                courses.Add(Course.parseFromFileLine(line));
+            }
+            courses.Sort();
+
+            lbCourses.Items.AddRange(courses.ToArray());
+        }
+
+        private void pbxAdd_Click(object sender, EventArgs e)
+        {
+            CreateCourse();
+        }
+
+        private void CreateCourse()
+        {
+            AddCourse addCourse = new AddCourse();
+            addCourse.Show();
+        }
     }
 }
