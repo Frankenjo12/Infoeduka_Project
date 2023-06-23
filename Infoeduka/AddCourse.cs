5572010ac3bc73d2ac5e2b7603dc450f4e017ecc
Amir Samahodaj <asamaho@racunarstvo.hr>
2023-06-23 22:55:15 +0200

    Add course and add notification implementation

--- Infoeduka/AddCourse.cs
+++ Infoeduka/AddCourse.cs
@@ -13,5 +13,27 @@ namespace Infoeduka
 {
     public partial class AddCourse : Form
     {
+        private const string PATHCOURSE = @"../../../Data/Courses.txt";
+
+        public AddCourse()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            Course course = new Course();
+            course.Name = tbName.Text;
+            course.Lecturer = tbLecturer.Text;
+            course.ECTS = int.Parse(tbECTS.Text);
+
+            if (!File.Exists(PATHCOURSE))
+            {
+                File.Create(PATHCOURSE);
+            }
+
+            File.AppendAllTextAsync(PATHCOURSE, Course.setForFileLine(course) + "\n");
+            this.Hide();
+        }
     }
 }
--- Infoeduka/AddNotification.cs
+++ Infoeduka/AddNotification.cs
@@ -13,5 +13,39 @@ namespace Infoeduka
 {
     public partial class AddNotification : Form
     {
+        private const string PATHNOTIF = @"../../../Data/Notifications.txt";
+        private const string PATHCOURSE = @"../../../Data/Courses.txt";
+
+        public AddNotification()
+        {
+            InitializeComponent();
+            if (!File.Exists(PATHCOURSE))
+            {
+                File.Create(PATHCOURSE);
+            }
+
+            string[] lines = File.ReadAllLines(PATHCOURSE);
+            List<Course> courses = new();
+            lines.ToList().ForEach(line => courses.Add(Course.parseFromFileLine(line)));
+
+            courses.ForEach(course => cbCourses.Items.Add(course.Name));
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            Notification notif = new Notification();
+            notif.Title = tbTitle.Text;
+            notif.DateTime = DateTime.Now;
+            notif.Course = cbCourses.SelectedItem.ToString();
+            notif.Message = tbMessage.Text;
+
+            if (!File.Exists(PATHNOTIF))
+            {
+                File.Create(PATHNOTIF);
+            }
+
+            File.AppendAllTextAsync(PATHNOTIF, Notification.setForFileLine(notif) + "\n");
+            this.Hide();
+        }
     }
 }
